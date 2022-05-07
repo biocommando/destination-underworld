658b2c00b5a9637efc76f9955f0a4a9dbef3ec3f
Joonas Salonpää <joonas1000@gmail.com>
2022-05-07 14:48:18 +0300

    Refactor boss 0 to use new .boss format features
    Also wrap boss-to-ini code generation into try-finally so that
    the intermediate debug file is always saved for debug purposes

--- dataloss/boss-to-ini.js
+++ dataloss/boss-to-ini.js
@@ -61,6 +61,14 @@ const override_event = (name, gameMode) => `[overrides__${name}__for_mode_${game
 
 const disable_event = (name, gameMode) => `on ${override_event(name, gameMode)} never: 0 do nothing: 0`
 
+const set_waypoint_sequence = seq => {
+    const result = []
+    for (let i = 0; i < seq.length; i++) {
+        result.push(`on waypoint_reached: ${seq[i]} do set_waypoint: ${seq[(i + 1) % seq.length]}`)
+    }
+    return result
+}
+
 const script_execute = fn => {
     fn()
     return ''
@@ -68,208 +76,209 @@ const script_execute = fn => {
 
 const intermediateBossFileForDebug = []
 
-fs
-    .readFileSync(fname)
-    .toString().replace(/\{#([^#]*)#\}/gm, (_, a) => {
-        const execute = script_execute
-        const result = eval(a)
-        if (result.map)
-            return result.join('\n')
-        return result
-    }).split(/\r?\n/)
-    .map(x => x.trim())
-    .filter(x => x[0] !== '/')
-    .filter(x => !Number(x))
-    .forEach(x => {
-        // main:
-        // set param = value
-        // events:
-        // on <trigger_type>: <trigger_value> do <event_type>: <param_name> = <param_value>, ...
-        // Events may have name:
-        // on [EventName] <trigger_type>: ...
-        // waypoints:
-        // def waypoint name = x, y
-        // spawnpoints:
-        // def spawnpoint name = x, y, probs
-        // preprocessor:
-        // ms(..) = calculate boss timer value for amount of milliseconds
-        // js$..$ = execute javascript
-        // internal parameters (e.g. filename):
-        // set_internal param = value
-        x = x.replace(/js\$([^$]+?)\$/g, (_, a) => {
+try {
+    fs
+        .readFileSync(fname)
+        .toString().replace(/\{#([^#]*)#\}/gm, (_, a) => {
             const execute = script_execute
-            return eval(a)
-        })
-        x = x.replace(/ms\(([\d]+?)\)/g, (_, a) => ms(a))
-        intermediateBossFileForDebug.push(x)
-        if (x.startsWith('on ')) {
-            x = x.replace('on ', '')
-            let evt = {}
-            x = x.replace(/\[([a-zA-Z0-9_]*)\]/, (_, name) => {
-                evt.name = name
-                return ''
+            const result = eval(a)
+            if (result.map)
+                return result.join('\n')
+            return result
+        }).split(/\r?\n/)
+        .map(x => x.trim())
+        .filter(x => x[0] !== '/')
+        .filter(x => !Number(x))
+        .forEach(x => {
+            // main:
+            // set param = value
+            // events:
+            // on <trigger_type>: <trigger_value> do <event_type>: <param_name> = <param_value>, ...
+            // Events may have name:
+            // on [EventName] <trigger_type>: ...
+            // waypoints:
+            // def waypoint name = x, y
+            // spawnpoints:
+            // def spawnpoint name = x, y, probs
+            // preprocessor:
+            // ms(..) = calculate boss timer value for amount of milliseconds
+            // js$..$ = execute javascript
+            // internal parameters (e.g. filename):
+            // set_internal param = value
+            x = x.replace(/js\$([^$]+?)\$/g, (_, a) => {
+                const execute = script_execute
+                return eval(a)
             })
-            const trigger_type = x.split(':')[0].trim()
-            let trigger_value = x.split(':')[1].split(' do ')[0].trim()
-            if (trigger_type === 'waypoint_reached') {
-                let wp = waypoints.find(y => y.name === trigger_value)
-
-                if (!wp && x.split(';')[1]) {
-                    wp = { name: x.trim(), x: x.split(';')[0].trim(), y: x.split(';')[1].trim(), value: getNextId() }
-                    waypoints.push(wp)
-                }
-                trigger_value = wp.value
-            }
-            const event_type = x.split(' do ')[1].split(':')[0].trim()
-            evt = { ...evt, trigger_type, trigger_value, event_type }
-            x = x.split(' do ')[1].split(':')[1]
-            if (event_type === 'set_waypoint') {
-                let wp = waypoints.find(y => y.name === x.trim())
-                if (!wp && x.split(';')[1]) {
-                    wp = { name: x.trim(), x: x.split(';')[0].trim(), y: x.split(';')[1].trim(), value: getNextId() }
-                    waypoints.push(wp)
-                }
-                evt.x = wp.x
-                evt.y = wp.y
-                evt.waypoint_id = wp.value
-            } else if (event_type === 'spawn') {
-                // Defining spawnpoint on the fly
-                if (x.trim().startsWith('(')) {
-                    x = x.replace('(', '').replace(')', '')
-                    const params = x.split(',').map(y => y.trim())
-                    const sp = {
-                        x: params[0], y: params[1],
-                        enemy_0_probability: params[2],
-                        enemy_1_probability: params[3],
-                        enemy_2_probability: params[4],
-                        enemy_3_probability: params[5],
-                        enemy_4_probability: params[6]
-                    }
-                    const existingSp = spawnpoints.find(sp2 => {
-                        sp.value = sp2.value
-                        sp.name = sp2.name
-                        return JSON.stringify(sp) === JSON.stringify(sp2)
-                    })
-                    if (existingSp) {
-                        evt.spawn_point = existingSp.value
-                    } else {
-                        sp.value = getNextId()
-                        delete sp.name
-                        if (params[7])
-                            sp.name = params[7]
-                        evt.spawn_point = sp.value
-                        spawnpoints.push(sp)
-                    }
-                } else {
-                    const sp = spawnpoints.find(y => y.name === x.trim())
-                    if (!sp)
-                        throw 'spawnpoint not found: ' + x
-                    evt.spawn_point = sp.value
-                }
-            } else if (event_type === 'inherit') {
-                evt.inheritAction = true
-            } else if (x && event_type !== 'nothing') {
-                const params = x.split(',').map(y => y.split('=').map(z => z.trim()))
-                params.forEach(p => {
-                    evt[p[0]] = p[1]
+            x = x.replace(/ms\(([\d]+?)\)/g, (_, a) => ms(a))
+            intermediateBossFileForDebug.push(x)
+            if (x.startsWith('on ')) {
+                x = x.replace('on ', '')
+                let evt = {}
+                x = x.replace(/\[([a-zA-Z0-9_]*)\]/, (_, name) => {
+                    evt.name = name
+                    return ''
                 })
+                const trigger_type = x.split(':')[0].trim()
+                let trigger_value = x.split(':')[1].split(' do ')[0].trim()
+                if (trigger_type === 'waypoint_reached') {
+                    let wp = waypoints.find(y => y.name === trigger_value)
+
+                    if (!wp && x.split(';')[1]) {
+                        wp = { name: x.trim(), x: x.split(';')[0].trim(), y: x.split(';')[1].trim(), value: getNextId() }
+                        waypoints.push(wp)
+                    }
+                    trigger_value = wp.value
+                }
+                const event_type = x.split(' do ')[1].split(':')[0].trim()
+                evt = { ...evt, trigger_type, trigger_value, event_type }
+                x = x.split(' do ')[1].split(':')[1]
+                if (event_type === 'set_waypoint') {
+                    let wp = waypoints.find(y => y.name === x.trim())
+                    if (!wp && x.split(';')[1]) {
+                        wp = { name: x.trim(), x: x.split(';')[0].trim(), y: x.split(';')[1].trim(), value: getNextId() }
+                        waypoints.push(wp)
+                    }
+                    evt.x = wp.x
+                    evt.y = wp.y
+                    evt.waypoint_id = wp.value
+                } else if (event_type === 'spawn') {
+                    // Defining spawnpoint on the fly
+                    if (x.trim().startsWith('(')) {
+                        x = x.replace('(', '').replace(')', '')
+                        const params = x.split(',').map(y => y.trim())
+                        const sp = {
+                            x: params[0], y: params[1],
+                            enemy_0_probability: params[2],
+                            enemy_1_probability: params[3],
+                            enemy_2_probability: params[4],
+                            enemy_3_probability: params[5],
+                            enemy_4_probability: params[6]
+                        }
+                        const existingSp = spawnpoints.find(sp2 => {
+                            sp.value = sp2.value
+                            sp.name = sp2.name
+                            return JSON.stringify(sp) === JSON.stringify(sp2)
+                        })
+                        if (existingSp) {
+                            evt.spawn_point = existingSp.value
+                        } else {
+                            sp.value = getNextId()
+                            delete sp.name
+                            if (params[7])
+                                sp.name = params[7]
+                            evt.spawn_point = sp.value
+                            spawnpoints.push(sp)
+                        }
+                    } else {
+                        const sp = spawnpoints.find(y => y.name === x.trim())
+                        if (!sp)
+                            throw 'spawnpoint not found: ' + x
+                        evt.spawn_point = sp.value
+                    }
+                } else if (event_type === 'inherit') {
+                    evt.inheritAction = true
+                } else if (x && event_type !== 'nothing') {
+                    const params = x.split(',').map(y => y.split('=').map(z => z.trim()))
+                    params.forEach(p => {
+                        evt[p[0]] = p[1]
+                    })
+                }
+                events.push(evt)
+            } else if (x.startsWith('set ')) {
+                let mainName = 'main'
+                x = x.replace(/\[([a-zA-Z0-9_]*)\]/, (_, name) => {
+                    mainName = name
+                    return ''
+                })
+                if (mainName !== 'main' && !mainSetup[mainName]) {
+                    mainSetup[mainName] = { ...main }
+                }
+                x = x.replace('set ', '').split('=').map(y => y.trim())
+                mainSetup[mainName][x[0]] = x[1]
+            } else if (x.startsWith('def waypoint')) {
+                x = x.replace('def waypoint', '').split('=').map(y => y.trim())
+                const params = x[1].split(',').map(y => y.trim())
+                waypoints.push({ name: x[0], x: params[0], y: params[1], value: getNextId() })
+            } else if (x.startsWith('def spawnpoint')) {
+                x = x.replace('def spawnpoint', '').split('=').map(y => y.trim())
+                const params = x[1].split(',').map(y => y.trim())
+                spawnpoints.push({
+                    name: x[0], x: params[0], y: params[1],
+                    enemy_0_probability: params[2],
+                    enemy_1_probability: params[3],
+                    enemy_2_probability: params[4],
+                    enemy_3_probability: params[5],
+                    enemy_4_probability: params[6],
+                    value: getNextId()
+                })
+            } else if (x.startsWith('set_internal ')) {
+                x = x.replace('set_internal ', '').split('=').map(y => y.trim())
+                internal[x[0]] = x[1]
+            } else if (x.startsWith('set_event_property ')) {
+                x = x.replace('set_event_property ', '')
+                const prop = x.split(':')[1].split('=').map(z => z.trim())
+                const evtName = x.split(':')[0]
+                events.filter(e => e.name && (e.name === evtName || e.name.startsWith('overrides__' + evtName + '__for_mode_')))
+                    .forEach(e => e[prop[0]] = prop[1])
             }
-            events.push(evt)
-        } else if (x.startsWith('set ')) {
-            let mainName = 'main'
-            x = x.replace(/\[([a-zA-Z0-9_]*)\]/, (_, name) => {
-                mainName = name
-                return ''
-            })
-            if (mainName !== 'main' && !mainSetup[mainName])
-            {
-                mainSetup[mainName] = {...main}
-            }
-            x = x.replace('set ', '').split('=').map(y => y.trim())
-            mainSetup[mainName][x[0]] = x[1]
-        } else if (x.startsWith('def waypoint')) {
-            x = x.replace('def waypoint', '').split('=').map(y => y.trim())
-            const params = x[1].split(',').map(y => y.trim())
-            waypoints.push({ name: x[0], x: params[0], y: params[1], value: getNextId() })
-        } else if (x.startsWith('def spawnpoint')) {
-            x = x.replace('def spawnpoint', '').split('=').map(y => y.trim())
-            const params = x[1].split(',').map(y => y.trim())
-            spawnpoints.push({
-                name: x[0], x: params[0], y: params[1],
-                enemy_0_probability: params[2],
-                enemy_1_probability: params[3],
-                enemy_2_probability: params[4],
-                enemy_3_probability: params[5],
-                enemy_4_probability: params[6],
-                value: getNextId()
-            })
-        } else if (x.startsWith('set_internal ')) {
-            x = x.replace('set_internal ', '').split('=').map(y => y.trim())
-            internal[x[0]] = x[1]
-        } else if (x.startsWith('set_event_property ')) {
-            x = x.replace('set_event_property ', '')
-            const prop = x.split(':')[1].split('=').map(z => z.trim())
-            const evtName = x.split(':')[0]
-            events.filter(e => e.name && (e.name === evtName || e.name.startsWith('overrides__' + evtName + '__for_mode_')))
-                .forEach(e => e[prop[0]] = prop[1])
-        }
-    })
-
-const objToIni = (obj, ignoreKeys = []) => {
-    return Object
-        .keys(obj)
-        .filter(key => !ignoreKeys.includes(key))
-        .map(key => key + '=' + obj[key])
-        .join('\r\n') + '\r\n'
-}
-
-let str = `# Generated from ${fname}\r\n`
-
-main.events = events.filter(e => !(e.name && e.name.startsWith('overrides__'))).length
-
-Object.keys(mainSetup).forEach(sect => {
-    str += `\r\n[${sect}]\r\n${objToIni(mainSetup[sect])}`
-})
-
-events.forEach(e => {
-    if (e.event_id !== undefined) {
-        const name = e.event_id
-        e.event_id = events.filter(e => !(e.name && e.name.startsWith('overrides__'))).findIndex(x => x.name === name)
-        if (e.event_id === -1)
-            throw 'event not found with name ' + name
-    }
-})
-
-events.filter(e => e.name && e.name.startsWith('overrides__'))
-    .forEach(e => {
-        const mainevt = events.find(e2 => e2.name === e.name.replace(/overrides__(.*)__for_mode_\d*/, '$1'))
-        if (!mainevt)
-            throw 'base event not found for override ' + e.name
-        if (e.trigger_type === 'inherit') {
-            e.trigger_type = mainevt.trigger_type
-            e.trigger_value = mainevt.trigger_value
-        }
-        mainevt['mode_override_' + e.name.split('__for_mode_')[1]] = e.name
-        if (e.inheritAction) {
-            e = {...mainevt, name: e.name, trigger_type: e.trigger_type, trigger_value: e.trigger_value}
-        }
-        Object.keys(e).forEach(key => {
-            if (key.startsWith('mode_override'))
-                delete e[key]
         })
-        str += `\r\n[${e.name}]\r\n${objToIni(e, ['name'])}`
+
+    const objToIni = (obj, ignoreKeys = []) => {
+        return Object
+            .keys(obj)
+            .filter(key => !ignoreKeys.includes(key))
+            .map(key => key + '=' + obj[key])
+            .join('\r\n') + '\r\n'
+    }
+
+    let str = `# Generated from ${fname}\r\n`
+
+    main.events = events.filter(e => !(e.name && e.name.startsWith('overrides__'))).length
+
+    Object.keys(mainSetup).forEach(sect => {
+        str += `\r\n[${sect}]\r\n${objToIni(mainSetup[sect])}`
     })
 
-events.filter(e => !(e.name && e.name.startsWith('overrides__'))).forEach((e, i) => {
-    str += `\r\n[event_${i}]\r\n`
-    str += objToIni(e, ['name'])
-})
+    events.forEach(e => {
+        if (e.event_id !== undefined) {
+            const name = e.event_id
+            e.event_id = events.filter(e => !(e.name && e.name.startsWith('overrides__'))).findIndex(x => x.name === name)
+            if (e.event_id === -1)
+                throw 'event not found with name ' + name
+        }
+    })
 
-spawnpoints.forEach((s, i) => {
-    str += `\r\n[spawn_point_${s.value}]\r\n`
-    str += objToIni(s, ['name', 'value'])
-})
+    events.filter(e => e.name && e.name.startsWith('overrides__'))
+        .forEach(e => {
+            const mainevt = events.find(e2 => e2.name === e.name.replace(/overrides__(.*)__for_mode_\d*/, '$1'))
+            if (!mainevt)
+                throw 'base event not found for override ' + e.name
+            if (e.trigger_type === 'inherit') {
+                e.trigger_type = mainevt.trigger_type
+                e.trigger_value = mainevt.trigger_value
+            }
+            mainevt['mode_override_' + e.name.split('__for_mode_')[1]] = e.name
+            if (e.inheritAction) {
+                e = { ...mainevt, name: e.name, trigger_type: e.trigger_type, trigger_value: e.trigger_value }
+            }
+            Object.keys(e).forEach(key => {
+                if (key.startsWith('mode_override'))
+                    delete e[key]
+            })
+            str += `\r\n[${e.name}]\r\n${objToIni(e, ['name'])}`
+        })
 
-fs.writeFileSync(internal.filename, str)
+    events.filter(e => !(e.name && e.name.startsWith('overrides__'))).forEach((e, i) => {
+        str += `\r\n[event_${i}]\r\n`
+        str += objToIni(e, ['name'])
+    })
 
-fs.writeFileSync('intermediate_debug.boss', intermediateBossFileForDebug.join('\r\n'))
+    spawnpoints.forEach((s, i) => {
+        str += `\r\n[spawn_point_${s.value}]\r\n`
+        str += objToIni(s, ['name', 'value'])
+    })
+
+    fs.writeFileSync(internal.filename, str)
+} finally {
+    fs.writeFileSync('intermediate_debug.boss', intermediateBossFileForDebug.join('\r\n'))
+}
