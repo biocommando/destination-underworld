53a396fd4ffb183334f0bfba629ae417767d2b4c
joonas <joonas1000@gmail.com>
2024-09-28 03:56:54 +0300

    Add one hand-tuned example of a play-tested random dungeon

---
+++ dataloss/Tunnels-of-Devil--m1l4tfxf/Tunnels-of-Devil--m1l4tfxf.bat
@@ -0,0 +1,6 @@
+
+IF EXIST mission1 (
+    cd ..
+    cd ..
+)
+DestinationUnderworld.exe --general--mission-pack=Tunnels-of-Devil--m1l4tfxf --Tunnels-of-Devil--m1l4tfxf--mission-count=4 --default-game-mode=1
---
+++ dataloss/Tunnels-of-Devil--m1l4tfxf/readme.txt
@@ -0,0 +1,3 @@
+Running the game with this custom mission pack:
+1. Copy the directory Tunnels-of-Devil--m1l4tfxf under dataloss directory
+2. Run the Tunnels-of-Devil--m1l4tfxf.bat file
