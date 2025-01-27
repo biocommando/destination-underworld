fef23f06f1c6930c6b4ba564750d7ee7b03f27c6
joonas <joonas1000@gmail.com>
2025-01-27 20:40:19 +0200

    loadindicator.h: add documentation

--- src/loadindicator.h
+++ src/loadindicator.h
@@ -1,3 +1,8 @@
 #pragma once
 
+/*
+ * Show a screen that indicates that the game is processing something that takes some time.
+ * The status argument is the text shown and if the display_load_state is 1, the screen
+ * contains also a progress bar.
+ */
 void progress_load_state(const char *status, int display_load_state);
