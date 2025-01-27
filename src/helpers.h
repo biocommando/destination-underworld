a4ea19b6480796edd631ebfea3dbd427da8bd43e
joonas <joonas1000@gmail.com>
2025-01-27 20:51:24 +0200

    helpers.h: add documentation

--- src/helpers.h
+++ src/helpers.h
@@ -2,8 +2,13 @@
 
 #include <time.h>
 
+/*
+ * Makes the game loop wait until the start of next animation frame.
+ * The wait is clock based so it should be pretty stable.
+ */
 void game_loop_rest(clock_t *state);
 
+// Structure representing a 2D point
 typedef struct coordinates
 {
     double x;
