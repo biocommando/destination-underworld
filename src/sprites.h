379fe50b9793d087dac64886061382736d91c97d
joonas <joonas1000@gmail.com>
2025-01-27 14:23:54 +0200

    sprites.h: add documentation

--- src/sprites.h
+++ src/sprites.h
@@ -24,15 +24,56 @@
 #define SPRITE_ID_MIN SPRITE_ID_BULLET
 #define SPRITE_ID_MAX SPRITE_ID_SKELETON
 
+// A struct for sprite definition in a spritesheet.
 typedef struct
 {
-    int sx, sy, width, height;
+    // Coordinates for the upper left corner of the sprite in the sprite sheet
+    int sx, sy;
+    // Dimensions for the sprite.
+    int width, height;
 } DuSprite;
 
+/*
+ * Set sprite definition in the sprite database with the given sprite_id.
+ */
 int set_sprite(int sprite_id, DuSprite sprite);
+/*
+ * Get sprite definition with the given id. If there's none with the id, returns NULL.
+ */
 DuSprite *get_sprite(int sprite_id);
+/*
+ * Reads sprites with ids min...max from provided file.
+ * Uses the record file format with the following syntax for a single sprite:
+ * sprite_[id] x=%d y=%d w=%d h=%d
+ *
+ * Note that the key values are just for human-readability, the key-value order needs to be exactly like this.
+ */
 int read_sprites_from_file(const char *filename, int min_id, int max_id);
+/*
+ * Draw a sprite from the sprite sheet on the screen. The x, y position is the upper left corner
+ * of the sprite.
+ */
 void draw_sprite(ALLEGRO_BITMAP *sprite_sheet, int sprite_id, int x, int y);
+/*
+ * Draw a sprite from the sprite sheet on the screen. The x, y position is the center
+ * of the sprite.
+ */
 void draw_sprite_centered(ALLEGRO_BITMAP *sprite_sheet, int sprite_id, int x, int y);
+/*
+ * Draw a sprite from the sprite sheet on the screen. The x, y position is the upper left corner
+ * of the sprite.
+ *
+ * Uses x/y offset which means that the sprite sheet coordinates are shifted by frame times
+ * width/height. This allows animating sprites with constant width/height without needing
+ * to create multiple sprite definitions.
+ */
 void draw_sprite_animated(ALLEGRO_BITMAP *sprite_sheet, int sprite_id, int x, int y, int frame_offset_x, int frame_offset_y);
+/*
+ * Draw a sprite from the sprite sheet on the screen. The x, y position is the center
+ * of the sprite.
+ *
+ * Uses x/y offset which means that the sprite sheet coordinates are shifted by frame times
+ * width/height. This allows animating sprites with constant width/height without needing
+ * to create multiple sprite definitions.
+ */
 void draw_sprite_animated_centered(ALLEGRO_BITMAP *sprite_sheet, int sprite_id, int x, int y, int frame_offset_x, int frame_offset_y);
