2b260c077f79bf38f8c84f121dea67e2811898a2
joonas <joonas1000@gmail.com>
2025-01-28 23:55:21 +0200

    allegro42_compat.h: Add documentation

--- src/allegro42_compat.h
+++ src/allegro42_compat.h
@@ -9,19 +9,64 @@
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 
+// Legacy compatibility
 #define rectfill(x, y, x2, y2, col) al_draw_filled_rectangle(x, y, x2 + 1, y2 + 1, col)
 
+// Legacy compatibility
 #define AL_PI 3.141592653589793
 
+/*
+ * Wait for event from allegro event queue and handle the event.
+ * Internally handles key up / down events (so that the key states
+ * can be just checked using the check_key function) and music playback.
+ *
+ * Returns 0 if waiting fails, 1 if event type is ALLEGRO_EVENT_TIMER and 2
+ * otherwise.
+ */
 int wait_event();
 
+/*
+ * Initializes allegro library. Also initializes the midi playback engine.
+ */
 int init_allegro();
+
+/*
+ * Frees all the memory reserved by the init_allegro function.
+ */
 void destroy_allegro();
+
+/*
+ * Waits for v "ticks".
+ */
 void wait_delay(int v);
+
+/*
+ * Waits for ms milliseconds. Note that wait resolution is "tick" which
+ * is 100 ms.
+ */
 void wait_delay_ms(int ms);
+
+/*
+ * Hang execution (not busy loop) until the key is pressed and released.
+ */
 void wait_key_press(int key);
+
+/*
+ * Hang execution (not busy loop) until any of the provided keys are pressed and released.
+ */
 int wait_key_presses(const int *keys, int num_keys);
+
+/*
+ * Wait until a key up event is received for the key.
+ */
 void wait_key_release(int key);
+
+/*
+ * Check if the key has been pressed but not yet released.
+ */
 int check_key(int key);
 
+/*
+ * Get the font initialized in init_allegro.
+ */
 ALLEGRO_FONT *get_font();
