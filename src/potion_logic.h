1017355512aa63726a0c4f858af5d2d1b7c8cd44
joonas <joonas1000@gmail.com>
2025-01-30 15:05:26 +0200

    potion_logic.h: add documentation

--- src/potion_logic.h
+++ src/potion_logic.h
@@ -2,4 +2,9 @@
 
 #include "world.h"
 
+/*
+ * Contains logic run each game frame for pickable potions.
+ * Checks if the potion is picked up and applies its effects and draws the sprites and
+ * the bubbling effect.
+ */
 void potion_logic(World *w);
