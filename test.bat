3f6a32328f0c3fad04a9cf05f186f0fdfa253774
joonas <joonas1000@gmail.com>
2025-02-14 03:36:49 +0200

    Re-record test cases

--- test.bat
+++ test.bat
@@ -1,13 +1,13 @@
 gcc test\test-du.c -o test-du.exe
-.\test-du.exe -ctest\complete-state--mission-2-mode-1.dat ^
--rtest\mission-2-mode-1.dat ^
--ctest\complete-state--mission-3-mode-200.dat ^
--rtest\mission-3-mode-200.dat ^
--ctest\complete-state--mission-5-mode-6.dat ^
--rtest\mission-5-mode-6.dat ^
--ctest\complete-state--mission-14-mode-0.dat ^
--rtest\mission-14-mode-0.dat ^
--ctest\complete-state--mission-100-mode-48.dat ^
--rtest\mission-100-mode-48.dat ^
--ctest\perks-change-mid-game-state.dat ^
--rtest\perks-change-mid-game.dat
+.\test-du.exe -ctest\normal-complete-state.dat ^
+-rtest\normal-recording.dat ^
+-ctest\brutally-hard-complete-state.dat ^
+-rtest\brutally-hard-recording.dat ^
+-ctest\overpowerup-complete-state.dat ^
+-rtest\overpowerup-recording.dat ^
+-ctest\potion-only-complete-state.dat ^
+-rtest\potion-only-recording.dat ^
+-ctest\powerup-only-complete-state.dat ^
+-rtest\powerup-only-recording.dat ^
+-ctest\perks-change-mid-level-complete-state.dat ^
+-rtest\perks-change-mid-level-recording.dat
