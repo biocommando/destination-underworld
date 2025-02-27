f8fb86804eec2b03b23ba744f38a584e4592956c
joonas <joonas1000@gmail.com>
2025-02-28 00:55:15 +0200

    predictableRandom.c: simplify logic

--- src/predictableRandom.c
+++ src/predictableRandom.c
@@ -11,24 +11,15 @@ static inline uint32_t get_pseudo_random_num(uint32_t state)
     return state;
 }
 
-static inline int predictable_random_next(int reset)
+static uint32_t state = 0;
+
+int pr_get_random()
 {
-    static uint32_t state = 0;
-    if (reset)
-    {
-        state = PREDICTABLE_RANDOM_SEED;
-        return 0;
-    }
     state = get_pseudo_random_num(state);
     return state & 0x7FFFFFFF;
 }
 
-int pr_get_random()
-{
-    return predictable_random_next(0);
-}
-
 void pr_reset_random()
 {
-    predictable_random_next(1);
+    state = PREDICTABLE_RANDOM_SEED;
 }
