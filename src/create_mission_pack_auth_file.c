3b739bc6930209a511cff9e12957ca4447fc1895
joonas <joonas1000@gmail.com>
2025-03-22 18:56:55 +0200

    mpauth: don't do debug prints when creating release so that the auth.dat file doesn't get polluted

--- src/create_mission_pack_auth_file.c
+++ src/create_mission_pack_auth_file.c
@@ -2,22 +2,27 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, int **argv)
+int main(int argc, char **argv)
 {
     if (argc < 3)
     {
         printf("Arguments required: mission pack name and path (with terminating slash, use slash instead of backslash)\n");
         return 1;
     }
+    int debug_prints = 1;
+    if (argc > 3 && !strcmp(argv[3], "no-debug-prints"))
+        debug_prints = 0;
 
     char node_cmd[512];
     sprintf(node_cmd, "node -e \""
         "s=fs.readdirSync('%s').join('\\n');fs.writeFileSync('files.txt',s)"
         "\"", argv[2]);
-    printf("Executing command:\n%s\n", node_cmd);
+    if (debug_prints)
+        printf("Executing command:\n%s\n", node_cmd);
     system(node_cmd);
     FILE *f = fopen("files.txt", "r");
-    printf("File contents:\n");
+    if (debug_prints)
+        printf("File contents:\n");
     while (!feof(f))
     {
         char path[256], fullpath[256];
@@ -31,6 +36,7 @@ int main(int argc, int **argv)
             path[strlen(path) - 1] = 0;
         sprintf(fullpath, "%s%s", argv[2], path);
         char hash[DMAC_SHA1_HASH_SIZE];
+        dmac_sha1_set_ctx(0);
         dmac_sha1_calculate_hash_f(hash, fullpath);
         char hex[100];
         convert_sha1_hash_to_hex(hex, hash);
