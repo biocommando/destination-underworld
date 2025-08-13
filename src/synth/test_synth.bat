428bc5bc9ace838b3eb5d82d28453521a32563a2
joonas <joonas1000@gmail.com>
2025-08-14 01:48:40 +0300

    Change synth test SHA hash after changing the optimization flag to Ofast

--- src/synth/test_synth.bat
+++ src/synth/test_synth.bat
@@ -8,5 +8,5 @@ echo ~~ Render track ~~
 echo ~~ Check SHA256 hash ~~
 certutil -hashfile ..\..\dataloss\midi-music\eetteritekno.mid.wav SHA256 > check
 
-node -e "sha=fs.readFileSync('check').toString().split(/\r?\n/)[1];console.log(sha === '03e0b818b54921c04acb09247a52974a5932e239fa4de8d8057c048527707a75' ? 'OK' : 'FAIL ' + sha)"
+node -e "sha=fs.readFileSync('check').toString().split(/\r?\n/)[1];console.log(sha === '058fd0b325f2697e104368cd5b9d0852789fa010c615424372927f82a231b56e' ? 'OK' : 'FAIL ' + sha)"
 del check
