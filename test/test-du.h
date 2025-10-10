d4d1bd1fb8c46d2d95121ab9f5b5ea1233844c67
Joonas Salonpää <joonas.salonpaa@vaisala.com>
2025-10-10 13:56:14 +0300

    test-du.h: Make unit test framework less magical
    Rely entirely on global state. Now the tests are simply functions:
    void test_name(){}
    
    And running them:
    
    RUN_TEST(test_name)
    
    This allows also splitting test execution to separate functions.

--- test/test-du.h
+++ test/test-du.h
@@ -2,11 +2,17 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct
+{
+    int test_result, n_tests, n_tests_failed, n_tests_skipped;
+    const char *filter_pos, *filter_neg;
+} T_test_state;
+
 #define _AB_ASSERT_BASE(type, fmt, a, b) \
     0;                                   \
     type _a = (a);                       \
     type _b = (b);                       \
-    sprintf(error, "A=" fmt ", B=" fmt, _a, _b);
+    sprintf(_test_error, "A=" fmt ", B=" fmt, _a, _b);
 
 #define _COMPARE(type, fmt, a, op, b) \
     _AB_ASSERT_BASE(type, fmt, a, b)  \
@@ -35,14 +41,15 @@
 #define ASSERT(condition)                                                     \
     do                                                                        \
     {                                                                         \
-        char error[1024] = "";                                                \
+        extern T_test_state _test_state;                                      \
+        char _test_error[1024] = "";                                          \
         int _condition_result = condition;                                    \
         if (!_condition_result)                                               \
         {                                                                     \
-            *_test_result = 0;                                                \
+            _test_state.test_result = 0;                                      \
             printf("-------- ASSERTION FAILED: " #condition "\n");            \
-            if (*error)                                                       \
-                printf("               >>> REASON: %s\n", error);             \
+            if (*_test_error)                                                 \
+                printf("               >>> REASON: %s\n", _test_error);       \
             extern _TEST_DEBUG_PRINT_VAR;                                     \
             if (*_test_debug_print)                                           \
                 printf("               >>>> DEBUG: %s\n", _test_debug_print); \
@@ -53,30 +60,21 @@
 
 #define EXPECT_FLOAT_EQ(a, b) EXPECT_FLOAT_EQ_DELTA(a, b, 1e-6)
 
-#define TEST_NAME(name) \
-    test_fn_##name
-
-#define TEST_FN_ARGS_DEF \
-    int *_test_result
-
-#define TEST_FN_DEF(name) \
-    int TEST_NAME(name)(TEST_FN_ARGS_DEF)
-
-#define TEST(name) \
-    TEST_FN_DEF(name)
-
-#define TEST_FN_ARGS _test_result
+// For backwards compatibility
+#define TEST(name) void name()
 
 #define INIT_TESTS                                                   \
-    int test_result, n_tests = 0, n_tests_failed = 0,                \
-                     n_tests_skipped = 0;                            \
-    const char *filter_pos = NULL, *filter_neg = NULL;               \
+    _test_state.n_tests = 0;                                         \
+    _test_state.n_tests_failed = 0;                                  \
+    _test_state.n_tests_skipped = 0;                                 \
+    _test_state.filter_pos = NULL;                                   \
+    _test_state.filter_neg = NULL;                                   \
     for (int i = 1; i < argc; i++)                                   \
     {                                                                \
         if (!strcmp(argv[i], "--include") && i + 1 < argc)           \
-            filter_pos = argv[i + 1];                                \
+            _test_state.filter_pos = argv[i + 1];                    \
         if (!strcmp(argv[i], "--exclude") && i + 1 < argc)           \
-            filter_neg = argv[i + 1];                                \
+            _test_state.filter_neg = argv[i + 1];                    \
         if (!strcmp(argv[i], "--help"))                              \
         {                                                            \
             printf("Run unit tests\nAccepted arguments:\n"           \
@@ -90,43 +88,46 @@
         }                                                            \
     }
 
-#define END_TESTS                                                               \
-    printf("\n\nTotal tests: %d, ok: %d, failed: %d, skipped: %d\n",            \
-           n_tests, n_tests - n_tests_failed, n_tests_failed, n_tests_skipped); \
-    printf("Test result: %s\n", n_tests_failed ? "FAIL" : "OK");                \
-    return n_tests_failed ? 1 : 0
+#define END_TESTS                                                                 \
+    printf("\n\nTotal tests: %d, ok: %d, failed: %d, skipped: %d\n",              \
+           _test_state.n_tests, _test_state.n_tests - _test_state.n_tests_failed, \
+           _test_state.n_tests_failed, _test_state.n_tests_skipped);              \
+    printf("Test result: %s\n", _test_state.n_tests_failed ? "FAIL" : "OK");      \
+    return _test_state.n_tests_failed ? 1 : 0
 
-#define TEST_FILTER(name)                        \
-    (filter_pos && !strstr(name, filter_pos)) || \
-        (filter_neg && strstr(name, filter_neg))
+#define TEST_FILTER(name)                                                \
+    (_test_state.filter_pos && !strstr(name, _test_state.filter_pos)) || \
+        (_test_state.filter_neg && strstr(name, _test_state.filter_neg))
 
-#define TEST_SUITE(name)                                                 \
-    int skip_suite_##name = TEST_FILTER(#name);                          \
+#define TEST_SUITE(name)                                                   \
+    int skip_suite_##name = TEST_FILTER(#name);                            \
     printf("\n%s suite " #name "\n", !skip_suite_##name ? "Run" : "Skip"); \
     if (!skip_suite_##name)
 
-#define RUN_TEST(name)                                 \
-    do                                                 \
-    {                                                  \
-        TEST_DEBUG_PRINT_CLEAR();                      \
-        if (TEST_FILTER(#name))                        \
-        {                                              \
-            n_tests_skipped++;                         \
-            break;                                     \
-        }                                              \
-        n_tests++;                                     \
-        test_result = 1;                               \
-        extern TEST_FN_DEF(name);                      \
-        TEST_NAME(name)(&test_result);                 \
-        n_tests_failed += test_result ? 0 : 1;         \
-        printf("%s : " #name "\n",                     \
-               test_result ? "[  OK  ]" : "[ FAIL ]"); \
+#define RUN_TEST(name)                                                 \
+    do                                                                 \
+    {                                                                  \
+        extern T_test_state _test_state;                               \
+        TEST_DEBUG_PRINT_CLEAR();                                      \
+        if (TEST_FILTER(#name))                                        \
+        {                                                              \
+            _test_state.n_tests_skipped++;                             \
+            break;                                                     \
+        }                                                              \
+        _test_state.n_tests++;                                         \
+        _test_state.test_result = 1;                                   \
+        extern void name();                                            \
+        name();                                                        \
+        _test_state.n_tests_failed += _test_state.test_result ? 0 : 1; \
+        printf("%s : " #name "\n",                                     \
+               _test_state.test_result ? "[  OK  ]" : "[ FAIL ]");     \
     } while (0)
 
 #define _TEST_DEBUG_PRINT_VAR char _test_debug_print[1024]
 
-#define TEST_GLOBAL_STATE \
-    _TEST_DEBUG_PRINT_VAR = "";
+#define TEST_GLOBAL_STATE       \
+    _TEST_DEBUG_PRINT_VAR = ""; \
+    T_test_state _test_state;
 
 #define TEST_DEBUG_PRINTF(...)                   \
     do                                           \
