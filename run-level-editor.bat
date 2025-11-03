683806c07d94fe52becb525398a4e02fe4c84cd3
joonas <joonas1000@gmail.com>
2025-11-03 22:09:15 +0200

    Change level editor mission pack argument syntax to be more sane

--- run-level-editor.bat
+++ run-level-editor.bat
@@ -2,4 +2,4 @@ cd  data
 echo "<body onload='open(`http://localhost:3000/editor/du-edit.html`, `_self`)'>" > open-du-edit.html
 open-du-edit.html
 
-call node editor/du-edit-server.js %1
+call node editor/du-edit-server.js %1 %2
